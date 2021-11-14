eb7869b8e97beb95a3a883084199c1205cf4dd4c
Chris N <hi.chrisn@gmail.com>
2021-11-14 16:42:12 -0300

    added

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log('Client side javascript file is loaded!');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const msgOne = document.querySelector('#msg-1');
+const msgTwo = document.querySelector('#msg-2');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  msgOne.textContent = 'Loading...';
+  msgTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
+      }
+    });
+  });
+  // console.log(location);
+});
