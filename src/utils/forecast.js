364666d907312b51af4ba8b6ed25e717f3a28937
Chris N <hi.chrisn@gmail.com>
2021-11-17 17:14:49 -0300

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location.')
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`);
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. The umidity is ${body.current.humidity}%.`);
     }
   });
 }
